bab7de8a81c76c9fa348407f79d2b3bc81aae60c
johntvu <120274177+johnvu124@users.noreply.github.com>
2024-03-11 15:39:53 -0400

    Create grid resize button

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 const container = document.querySelector(".container")
 
-
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let newDiv = document.createElement('div')
-
-        newDiv.classList.add('container-item')
-
-        container.appendChild(newDiv)
+function createGrid (num) {
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            let newDiv = document.createElement('div')
+    
+            newDiv.classList.add('container-item')
+            newDiv.style.flex = `calc(100% / ${num})`
+            newDiv.style.minHeight = `calc(100% / ${num})`
+    
+            container.appendChild(newDiv)
+        }
     }
+
+    const item = document.querySelectorAll(".container-item")
+
+    item.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = 'black'
+        })
+    })
 }
 
-const item = document.querySelectorAll(".container-item")
+const newGrid = document.querySelector(".newGrid")
 
-item.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'black'
-    })
-})
+newGrid.addEventListener('click', () => {
+    let userNum = prompt('Number of squares per side (1-100)')
+
+    container.replaceChildren()
+
+
+    if (userNum > 0 && userNum < 100) {
+        createGrid(userNum)
+    } else {
+        userNum = prompt('Number of squares per side **(1-100)')
+    }
+
+    createGrid(userNum)
+
+} )
+
+
+createGrid(16)
